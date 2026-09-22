5a57246c9b2ebeb0b71c689986e094ce102f37f8
ikaro santos <ikarosantos2016@gmail.com>
2026-09-22 10:09:53 -0300

    aula de tratamento de exessões

---
+++ Atrazei_aula_poo/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    int a = int.Parse("2");
+    System.Console.WriteLine("Digite um valor de preferencia invalido para teste: ");
+    int idade = int.Parse(System.Console.ReadLine());
+
+    if (idade < 0)
+    {
+        throw new IdadeInvalidaException("Idade não pode ser negativa");
+    }
+}
+catch (IdadeInvalidaException ex)
+{
+    System.Console.WriteLine(ex.Message);
+}
+catch (FormatException )
+{
+    System.Console.WriteLine("Erro de formato!");   
+}
+catch (Exception )
+{
+    System.Console.WriteLine("Erro genérico:");
+}
+finally
+{
+    System.Console.WriteLine("teste de finally");
+}
